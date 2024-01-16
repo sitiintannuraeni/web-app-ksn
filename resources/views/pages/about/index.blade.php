c5a15f932e7e3f92d96e45100acf6124fe009c00
sitiintannuraeni <sitiintannuraeni@smkwikrama.sch.id>
2024-01-16 07:55:43 +0700

    add about

--- resources/views/pages/about/index.blade.php
+++ resources/views/pages/about/index.blade.php
@@ -1,13 +1,14 @@
 <div class="ksn__about-us">
-    <div class="container py-5">
-        <h2 class="fw-bold text-center">ABOUT US</h2>
-
-        <div class="card">
-            <div class="card-body">
-                <h2 class="fw-bold">Kreasi Swala Nusantara</h2>
-                <p>merupakan sebuah start up yang bergerak dibidang industri kreatif dan pengembangan bisnis skala
-                    mikro kecil menengah yang memiliki sub bisnis pengembangan aplikasi, pengembangan sebuah brand,
-                    manajemen sosial media dan konsultan dalam industri kreatif.</p>
+    <div class="container position-relative h-100">
+        <div class="d-flex h-100 align-items-center justify-content-center">
+            <div class="row flex-lg-row-reverse align-items-center">
+                <div class="col-10 col-sm-8 col-lg-6">
+                    <img src="https://getbootstrap.com/docs/5.2/examples/heroes/bootstrap-themes.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+                </div>
+                <div class="col-lg-6">
+                    <h1 class="fw-semibold lh-1 mb-3">Tentang Kami</h1>
+                    <p class="fw-light">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -1,15 +1,15 @@
-    <section id="home">
-        <div class="circle"> </div>
-        <div class="content">
-            <div class="textBox">
-                <h2>CV KREASI SWALA NUSANTARA (KSN)</h2>
-                <p>Apa itu CV? CV merupakan bentuk usaha yang bukan berbadan hukum karena tidak ada peraturan tertentu
-                    yang
-                    mengaturnya</p>
-                <a href="about.html" class="btn-more">Selengkapnya</a>
-            </div>
-            <div class="imgBox">
-                <img src="{{asset('assets/logo.png')}}" alt="Cake" class="cake" id="image">
-            </div>
+<section id="home">
+    <div class="circle"> </div>
+    <div class="content">
+        <div class="textBox d-flex flex-column align-items-start gap-3">
+            <h1 class="fw-semibold">CV KREASI SWALA NUSANTARA (KSN)</h1>
+            <p class="fw-light">Apa itu CV? CV merupakan bentuk usaha yang bukan berbadan hukum karena tidak ada peraturan tertentu
+                yang
+                mengaturnya</p>
+            <a href="about.html" class="btn btn-primary rounded-5 py-2 px-4">Selengkapnya</a>
         </div>
-    </section>
+        <div class="imgBox d-none">
+            <img src="{{asset('assets/logo.png')}}" alt="Cake" class="cake" id="image">
+        </div>
+    </div>
+</section>
